82d25dcd9516fc88146d205bccccf21ba191ca93
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2023-03-19 23:28:36 -0300

    NEW FEATURES

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+// import '../assets';
+
+import '../assets/js/main.js'
+
+import.meta.glob([ '../assets/**', ]);
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
