52d1dd7ad1b0a51f5cbefa93561023fd574da810
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2023-04-17 19:00:57 -0300

    [FINISH] ABOUT

--- app/Http/Livewire/About.php
+++ app/Http/Livewire/About.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Language;
 use App\Models\Profile;
 use Livewire\Component;
 
@@ -9,6 +10,6 @@ class About extends Component
 {
     public function render()
     {
-        return view('livewire.about', ['profile' => Profile::first()]);
+        return view('livewire.about', ['profile' => Profile::first(), 'languages' => Language::orderBy('name')->get()]);
     }
 }
